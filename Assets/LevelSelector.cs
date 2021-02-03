16856b5b3e390bf47584a112eedb58e00e4dbcb8
verose27 <71091172+verose27@users.noreply.github.com>
2021-02-03 08:51:03 +0100

    Level_selection

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector: MonoBehaviour
+{
+    public Button[] lvlButtons;
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2);
+        for(int i=0; i<lvlButtons.Length; i++)
+        {
+            if (i + 3 > levelAt)
+                lvlButtons[i].interactable = false;
+        }
+    }
+}
