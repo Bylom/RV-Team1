e4ded006eb97571e66bbfdb7e3cac15131145b35
DESKTOP-H8HC8N0\amede <amedeofresia@gmail.com>
2021-02-05 00:27:49 +0100

    Add: introduzione, animazioni astronauta

---
+++ Assets/Scripts/Intro/LoadFirstScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ciaoooo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(2);
+        }
+        
+    }
+}
