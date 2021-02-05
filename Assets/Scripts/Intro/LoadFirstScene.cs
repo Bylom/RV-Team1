e4a66a964c1d5871716bd951a3985b3426a467d6
DESKTOP-H8HC8N0\amede <amedeofresia@gmail.com>
2021-02-05 21:17:18 +0100

    Add: Terrain prova
    ho creato una nuova scena PROVA

--- Assets/Scripts/Intro/LoadFirstScene.cs
+++ Assets/Scripts/Intro/LoadFirstScene.cs
@@ -8,7 +8,7 @@ public class LoadFirstScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("ciaoooo");
+
     }
 
     // Update is called once per frame
