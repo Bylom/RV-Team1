f623373fc665b24edda1bf502c3b18f92a07cea1
DESKTOP-H8HC8N0\amede <amedeofresia@gmail.com>
2021-02-14 16:21:14 +0100

    Fix: scena materiali, cartelle audio

--- Assets/Scripts/Inventory/RockCount.cs
+++ Assets/Scripts/Inventory/RockCount.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class RockCount : MonoBehaviour
 {
+    private Animator opening;
 
-    int rockCount = 0;
+    public int rockCount = 0;
+    public int ballCount = 0;
+
+    private static readonly int Open = Animator.StringToHash("Open");
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        opening = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        void OnTriggerStay(Collider rock)
-        {
-            if (rock.gameObject.CompareTag("Rock"))
-            {
+        
+    }
 
-                if (Input.GetKey(KeyCode.E))
-                {
-                    Debug.Log("presaaaaaa");
-                    //rock.gameObject.SetActive(false);
-                    rockCount++;
-                }
+    void OnTriggerStay(Collider rock)
+    {
+        if (rock.gameObject.CompareTag("Rock"))
+        {
+            opening.SetBool("Open", true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("presaaaaaa");
+                rock.gameObject.SetActive(false);
+                rockCount++;
+            }
+        }
+
+        if (rock.gameObject.CompareTag("Palla"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("pallaaaaa");
+                rock.gameObject.SetActive(false);
+                ballCount++;
             }
         }
     }
